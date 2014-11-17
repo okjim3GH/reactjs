56a8eef05140dc481168e9204ee3467e30b46174
Jim O'Keefe <jim@jimokeefe.net>
2014-11-17 11:05:58 -0500

    Massive update

--- restservice/server.js
+++ restservice/server.js
@@ -4,15 +4,17 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');    // call express
-var zapp        = express();         // define our app using express
+var express    = require('express'); // call express
+var cors       = require('cors');
+var app        = express();         // define our app using express
 var bodyParser = require('body-parser');
 var Comment    = require('./models/comment');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-zapp.use(bodyParser.urlencoded({ extended: true }));
-zapp.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 var port = process.env.PORT || 8080;    // set our port
 
@@ -79,13 +81,67 @@ router.route('/comments')
     });
   });
 
+  router.route('/comments/:comment_id')
+
+  // get the comment with that id (accessed at GET http://localhost:8080/api/comments/:comment_id)
+  .get(function(req, res) {
+    Comment.findById(req.params.comment_id, function(err, comment) {
+      if (err)
+        res.send(err);
+      res.json(comment);
+    });
+  })
+
+  .put(function(req, res) {
+
+    // use our comment model to find the comment we want
+    Comment.findById(req.params.comment_id, function(err, comment) {
+
+      if (err)
+        res.send(err);
+      
+      // update the comments info
+    
+      if(req.body.author !== undefined){
+        comment.author = req.body.author;
+      }
+      if(req.body.text !== undefined){
+        comment.text = req.body.text;  // update the comments info
+      }
+      if(req.body.key !== undefined){
+        comment.key = req.body.key;  // update the comments info
+      }
+
+
+      // save the comment
+      comment.save(function(err) {
+        if (err)
+          res.send(err);
+
+        res.json({ message: 'Comment updated!' });
+      });
+
+    });
+  })
+
+  .delete(function(req, res) {
+    Comment.remove({
+      _id: req.params.comment_id
+    }, function(err, comment) {
+      if (err)
+        res.send(err);
+
+      res.json({ message: 'Successfully deleted' });
+    });
+  });
+
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-zapp.use('/api', router);
+app.use('/api', router);
 
 
 
 // START THE SERVER
 // =============================================================================
-zapp.listen(port);
+app.listen(port);
 console.log('Magic happens on port ' + port);
